9da57f442e4a3f0d9c095a8abc415951206a4964
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-11-22 18:39:43 +0700

    chore: hide Gallery Section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -30,7 +30,7 @@ const Index = () => {
             <CoupleSection />
           </div>
           <EventSection />
-          <GallerySection />
+          {/* <GallerySection /> */}
           <RsvpSection />
           <GiftSection />
           <Footer />
